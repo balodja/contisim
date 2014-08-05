a0bee81170c58fa5acdd935eb34d5d6cb65587fb
Vladimir Korolyov <balodja@gmail.com>
2014-08-06 02:53:15 +0400

    Self contained example.

---
+++ src/SelfContainedExample.hs
@@ -0,0 +1,202 @@
+{-# LANGUAGE NoImplicitPrelude, BangPatterns, ExistentialQuantification #-}
+{-# LANGUAGE NoImplicitPrelude, Arrows, IncoherentInstances, Rank2Types #-}
+{-# LANGUAGE FlexibleInstances, MultiParamTypeClasses #-}
+{-# OPTIONS_GHC -fno-warn-name-shadowing -fno-warn-orphans #-}
+import NumericPrelude
+import Control.Arrow
+import Control.Applicative (Applicative, pure, (<*>), liftA, liftA2)
+import qualified Control.Category as Cat
+
+import qualified Algebra.Additive as Additive
+import qualified Algebra.Algebraic as Algebraic
+import qualified Algebra.Field as Field
+import qualified Algebra.Module as Module
+import qualified Algebra.Ring as Ring
+import qualified Algebra.Transcendental as Transcendental
+import qualified Algebra.VectorSpace as VectorSpace
+
+-- Arrow-based streams are the building blocks for simulation
+
+data Continuous t a b = forall s. Continuous !s (t -> a -> s -> (b, s))
+
+simStep :: t -> a -> (Continuous t a b) -> (b, Continuous t a b)
+simStep dt a (Continuous s f) = let !(b, s') = f dt a s in (b, Continuous s' f)
+
+simTrace :: t -> [a] -> (Continuous t a b) -> [b]
+simTrace dt (x:xs) f = let !(y, f') = simStep dt x f in y `seq` (y : simTrace dt xs f')
+simTrace _ [] _ = []
+
+instance Cat.Category (Continuous t) where
+  id = Continuous () (\_ x _ -> (x, ()))
+  Continuous sg g . Continuous sf f = Continuous (sg, sf) $ \dt x (!sg, !sf) ->
+    let !(y, sf') = f dt x sf
+        !(z, sg') = g dt y sg
+    in (z, (sg', sf'))
+
+instance Arrow (Continuous t) where
+  arr f = Continuous () (\_ x _ -> (f x, ()))
+  
+  first (Continuous sf f) = Continuous sf $ \dt (x, r) !s ->
+    let !(y, s') = f dt x s
+    in ((y, r), s')
+
+  second (Continuous sf f) = Continuous sf $ \dt (r, x) !s ->
+    let !(y, s') = f dt x s
+    in ((r, y), s')
+
+  (Continuous sf f) *** (Continuous sg g) = Continuous (sf, sg) $ \dt (xf, xg) (!sf, !sg) ->
+    let !(yf, sf') = f dt xf sf
+        !(yg, sg') = g dt xg sg
+    in ((yf, yg), (sf', sg'))
+
+  (Continuous sf f) &&& (Continuous sg g) = Continuous (sf, sg) $ \dt x (!sf, !sg) ->
+    let !(yf, sf') = f dt x sf
+        !(yg, sg') = g dt x sg
+    in ((yf, yg), (sf', sg'))
+
+instance ArrowLoop (Continuous t) where
+  loop (Continuous s f) = Continuous s $ \dt x !s ->
+    let (!(y, r), s') = f dt (x, r) s
+    in (y, s')
+
+
+-- Signals are datatypes to represent current signal value
+-- with some extra possibilities (e.g. first-second order derivatives)
+
+class Applicative s => Signal s
+
+-- Signals are the thing, that can be integrated
+class Signal s => Integrable s where
+  integrate :: VectorSpace.C t a => t -> (s a) -> a -> (s a, a)
+
+-- Extract current value of the signal
+class Signal s => Extractable s where
+  extractS :: s a -> a
+
+instance (Signal s, Additive.C a) => Additive.C (s a) where
+  zero = pure zero
+  negate = liftA negate
+  (+) = liftA2 (+)
+  (-) = liftA2 (-)
+
+instance (Signal s, Ring.C a) => Ring.C (s a) where
+  (*) = liftA2 (*)
+  one = pure one
+  fromInteger = pure . fromInteger
+
+instance (Signal s, Field.C a) => Field.C (s a) where
+  (/) = liftA2 (/)
+  recip = liftA recip
+  fromRational' = pure . fromRational'
+  (^-) x p = liftA (^- p) x
+
+instance (Signal s, Module.C a v) => Module.C (s a) (s v) where
+  (*>) = liftA2 (*>)
+
+instance (Signal s, Algebraic.C a) => Algebraic.C (s a) where
+  sqrt = liftA sqrt
+  root x = liftA (Algebraic.root x)
+  (^/) x p = liftA (^/ p) x
+
+instance (Signal s, Transcendental.C a) => Transcendental.C (s a) where
+  pi = pure pi
+  exp = liftA exp
+  log = liftA log
+  logBase = liftA2 logBase
+  (**) = liftA2 (**)
+  sin = liftA sin
+  tan = liftA tan
+  cos = liftA cos
+  asin = liftA asin
+  atan = liftA atan
+  acos = liftA acos
+  sinh = liftA sinh
+  tanh = liftA tanh
+  cosh = liftA cosh
+  asinh = liftA asinh
+  atanh = liftA atanh
+  acosh = liftA acosh
+
+-- Signal data with Euler method integration, and instances for it.
+
+newtype EU1 v = EU1 v deriving (Show, Read, Eq)
+
+instance Functor EU1 where
+  fmap f (EU1 x) = EU1 (f x)
+
+instance Applicative EU1 where
+  pure x = EU1 x
+  (EU1 f) <*> (EU1 x) = EU1 (f x)
+  
+instance Signal EU1
+
+instance Integrable EU1 where
+  integrate dt (EU1 x') x0 = let x = x0 + dt *> x' in (EU1 x0, x)
+
+instance Extractable EU1 where
+  extractS (EU1 x) = x
+
+
+-- Signal data with Runge-Kutta integration, with instances.
+
+data RK4 v = RK4 !v v v v deriving (Show, Read, Eq)
+
+instance Functor RK4 where
+  fmap f (RK4 x1 x2 x3 x4) = RK4 (f x1) (f x2) (f x3) (f x4)
+
+instance Applicative RK4 where
+  pure x = RK4 x x x x
+  (RK4 f1 f2 f3 f4) <*> (RK4 x1 x2 x3 x4) = RK4 (f1 x1) (f2 x2) (f3 x3) (f4 x4)
+
+instance Signal RK4
+
+instance Integrable RK4 where
+  integrate = let two = fromRational 2
+                  six = fromRational 6
+              in \dt (~(RK4 x1' x2' x3' x4')) x0 ->
+              (RK4 x0 (x0 + (dt / two) *> x1') (x0 + (dt / two) *> x2') (x0 + dt *> x3'),
+               x0 + (dt / six) *> (x1' + two *> x2' + two *> x3' + x4'))
+
+instance Extractable RK4 where
+  extractS = \(RK4 x _ _ _) -> x
+
+-- Just a wrapper to make an arrow from "integrate"
+
+integrator :: (Integrable s, VectorSpace.C t a) => a -> Continuous t (s a) (s a)
+integrator x0 = Continuous x0 integrate
+
+-- Simple sine wave as an example
+
+sine :: (Signal s, Integrable s) => Continuous Double () (s Double)
+sine = proc _ -> do
+  rec x <- integrator 0.0 -< y
+      y <- integrator 1.0 -< liftA negate x
+  returnA -< x
+
+{-
+vdp :: (Signal s, Integrable s) => Continuous Double (s ()) (s Double)
+vdp = proc _ -> do
+  rec x <- integratorS 0.0 -< x - liftA (/ 3) (x ^ 3) - y
+      y <- integratorS 1.0 -< x
+  returnA -< x
+-}
+
+-- Run an arrow
+
+runIt :: Double -> Int -> (forall s. (Signal s, Integrable s) => Continuous Double () (s Double)) -> [(Double, Double, Double)]
+runIt step n block =
+  let input = repeat ()
+      outputRK = simTrace step input block :: [RK4 Double]
+      outputEU = simTrace step input block :: [EU1 Double]
+      times = map (* step) [0, 1 ..] :: [Double]
+  in take (succ n) $ zip3 times (map extractS outputEU) (map extractS outputRK)
+
+-- Show the results of the run
+
+showIt :: [(Double, Double, Double)] -> String
+showIt = unlines . map (\(d1, d2, d3) -> unwords . map show $ [d1, d2, d3])
+
+-- Do it
+
+main :: IO ()
+main = writeFile "sine.dat" (showIt $ runIt (pi / 10) 10 sine)
